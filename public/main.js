47eef3686d444347009d9f7c17eb88c99fb40aab
Vera Kippel <veki2400@student.miun.se>
2025-04-27 17:30:41 +0200

    tar bort kod som inte ska vara i uppgiften

--- public/main.js
+++ public/main.js
@@ -1,9 +1,1 @@
-const express = require("express");
-const bodyParser = require("body-parser"); //Möjlighet att läsa in från form-data
-const cors = require("cors");
-
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true})); //Ta emot form-data
 
